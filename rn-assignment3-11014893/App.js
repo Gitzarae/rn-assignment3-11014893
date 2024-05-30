77c72cb1bf28468ece0a5bb8030a32464c87a0d1
ADK <adk.kda10@gmail.com>
2024-05-30 16:52:13 +0000

    Second component created

--- rn-assignment3-11014893/App.js
+++ rn-assignment3-11014893/App.js
@@ -4,13 +4,17 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
+      <StatusBar style="auto" />
       <View style={styles.container1}>
         <Text style={styles.text1}>Hello , Devs</Text>
         <Text style={styles.text2}>14 Tasks today</Text>
         <View style={styles.round}></View>
       </View>
-      <View style={styles.container2}></View>
-      <StatusBar style="auto" />
+      <View style={styles.container2}>
+        <View style={styles.rectangle1}></View>
+        <View style={styles.rectangle2}></View>
+      </View>
+      <Text style={styles.textt}>Categories</Text>
     </View>
   );
 }
@@ -28,6 +32,7 @@ const styles = StyleSheet.create({
     width: "90%",
     backgroundColor: "pink",
     paddingHorizontal: 2,
+    marginTop: 40,
     marginBottom: 20,
     alignItems: "center",
     justifyContent: "space-between",
@@ -58,10 +63,30 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     height: "10%",
     width: "90%",
-    marginBottom: 600,
+    marginBottom: "",
     backgroundColor: "yellow",
     alignItems: "center",
     justifyContent: "space-between",
     flexWrap: "",
+  },
+  rectangle1: {
+    backgroundColor: "green",
+    width: 280,
+    height: 70,
+    borderRadius: 20,
+    marginLeft: 2,
+  },
+  rectangle2: {
+    backgroundColor: "green",
+    width: 70,
+    height: 70,
+    borderRadius: 25,
+    marginRight: 2,
+  },
+  textt: {
+    fontSize: "30px",
+    marginTop: 50,
+    marginBottom: 520,
+    marginLeft:-225,
   },
 });
